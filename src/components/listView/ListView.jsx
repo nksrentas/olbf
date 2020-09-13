bea0dc2a24d52acdb570f6f8ecd6ad8e2379ef6d
nksrentas <cse2343@gmail.com>
2020-09-13 13:02:41 +0300

    responsive list view

--- src/components/listView/ListView.jsx
+++ src/components/listView/ListView.jsx
@@ -1,28 +1,8 @@
 import React from 'react';
-
-import test from '../../test.svg';
+import ListViewItem from './ListViewItem';
 
 const ListView = () => {
-  return (
-    <div className='col-md-12'>
-      <div className='card flex-row flex-wrap'>
-        <div className='card-header border-0'>
-          <img src={test} alt='' />
-        </div>
-        <div className='card-block px-2'>
-          <h4 className='card-title'>Title</h4>
-          <p className='card-text'>Description</p>
-          <a href='/' class='btn btn-primary'>
-            BUTTON
-          </a>
-        </div>
-        <div className='w-100'></div>
-        <div className='card-footer w-100 text-muted'>
-          Footer stating cats are CUTE little animals
-        </div>
-      </div>
-    </div>
-  );
+  return <ListViewItem />;
 };
 
 export default ListView;
