5a4695cb6a6ab9786432c417e0029650fdd65307
nksrentas <cse2343@gmail.com>
2020-09-14 22:07:52 +0300

    fix navigation margin

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -37,7 +37,7 @@ function Navigation(props) {
   return (
     <>
       <div
-        className='nav flex-column nav-pills'
+        className='nav flex-column nav-pills mt-5'
         id='v-pills-tab'
         role='tablist'
         aria-orientation='vertical'
